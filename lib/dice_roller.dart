85bd8429426746c31710d380f5e827068c97997d
Kartikeya Singh <singhkartikeya200@gmail.com>
2023-07-15 21:17:37 +0530

    Dice Roll App Added

---
+++ lib/dice_roller.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+ final randomizer = Random();
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  // var activeDiceImage = 'assets/images/dice-4.png';
+ 
+  var currentdiceroll = 2;
+  void rollDice() {
+    setState(() {
+      currentdiceroll = randomizer.nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/dice-$currentdiceroll.png',
+          width: 200,
+        ),
+        const SizedBox(height: 20),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            // padding: const EdgeInsets.only(
+            //   top: 20,
+            // ),
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(
+              fontSize: 28,
+            ),
+          ),
+          child: const Text('Roll Dice'),
+        )
+      ],
+    );
+  }
+}
